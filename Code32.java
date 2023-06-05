cf163316a2b622f2e1580ca62dbdfabe56398e8b
yuqui <kobayukapc@gmail.com>
2023-06-05 12:01:49 +0900

    Code32追加

---
+++ Code32.java
@@ -0,0 +1,9 @@
+public class Code32{
+	public static void main(String[] args){
+		boolean doorClose=true;
+		while(doorClose==true){
+			System.out.println("ノックする");
+			System.out.println("１分待つ");
+		}
+	}
+}
