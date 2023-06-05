8ed132322971198b80fc6fc324fb2269a5e66917
maeyama <Cralia.kzhr@gmail.com>
2023-06-05 11:09:58 +0900

    歳を間違えていたので修正

--- Code213.java
+++ Code213.java
@@ -2,6 +2,6 @@ public class Code213{
 	public static void main(String[] args){
 		String age="31";
 		int n=Integer.parseInt(age);
-		System.out.println("あなたは来年、"+(n+1)+"際になりますね。");
+		System.out.println("あなたは来年、"+(n+1)+"歳になりますね。");
 	}
 }
